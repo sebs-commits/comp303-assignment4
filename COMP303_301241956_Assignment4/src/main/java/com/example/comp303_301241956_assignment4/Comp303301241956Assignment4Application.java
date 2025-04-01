f7a4f9178ad1e120474f2608c859f87574aaf1ea
Sebastian <sebastian_bdavid@hotmail.com>
2025-04-01 08:43:54 -0400

    init frontend

--- COMP303_301241956_Assignment4/src/main/java/com/example/comp303_301241956_assignment4/Comp303301241956Assignment4Application.java
+++ COMP303_301241956_Assignment4/src/main/java/com/example/comp303_301241956_assignment4/Comp303301241956Assignment4Application.java
@@ -3,8 +3,10 @@ package com.example.comp303_301241956_assignment4;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication( exclude = {SecurityAutoConfiguration.class})
+@CrossOrigin(origins = "*")
 public class Comp303301241956Assignment4Application {
 
     public static void main(String[] args) {
