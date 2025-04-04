644d75da383ff95e1a556590c46dcbf3055e9850
Sebastian <sebastian_bdavid@hotmail.com>
2025-04-04 11:18:57 -0400

    feat: seperated staff details into its own component

---
+++ COMP303_301241956_Assignment4_Frontend/client/src/components/StaffDetails.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const StaffDetails = ({ staffData }) => {
+  return (
+    <div className="card bordered bg-base-100 shadow-xl">
+      <div className="card-body">
+        <h2 className="card-title">Staff Information</h2>
+        <div className="divider"></div>
+        <p>
+          <strong>ID:</strong> {staffData.staffId}
+        </p>
+        <p>
+          <strong>Name:</strong> {staffData.staffName}
+        </p>
+        <p>
+          <strong>Rating:</strong>{" "}
+          {staffData.staffRating === 0 ? "No Rating" : staffData.staffRating}
+        </p>
+        <p>
+          <strong>Department:</strong>{" "}
+          {staffData.department ? staffData.department.departmentName : "N/A"}
+        </p>
+        <p>
+          <strong>Assigned Hotel:</strong>{" "}
+          {staffData.hotel ? staffData.hotel.hotelName : "N/A"}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default StaffDetails;
