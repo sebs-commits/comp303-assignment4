dcd968f5483c1f6600bc95c984f3fd3238a0a1d2
Sebastian <sebastian_bdavid@hotmail.com>
2025-04-04 17:20:52 -0400

    refactor: allow all origins for now

--- COMP303_301241956_Assignment4/src/main/java/com/example/comp303_301241956_assignment4/controller/HotelController.java
+++ COMP303_301241956_Assignment4/src/main/java/com/example/comp303_301241956_assignment4/controller/HotelController.java
@@ -5,6 +5,7 @@ import com.example.comp303_301241956_assignment4.repository.HotelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*")
 public class HotelController {
 
     @Autowired
