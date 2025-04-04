e25404c655d28dca3bc93f5d62214e044f26f3f1
Sebastian <sebastian_bdavid@hotmail.com>
2025-04-04 10:47:21 -0400

    feat: added react router dom for routing

--- COMP303_301241956_Assignment4_Frontend/client/src/App.jsx
+++ COMP303_301241956_Assignment4_Frontend/client/src/App.jsx
@@ -1,11 +1,22 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
 import StaffTable from "./components/StaffTable";
+import StaffDetailPage from "./pages/StaffDetailPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
       <NavBar />
-      <StaffTable />
+      {/* 4. Use BrowserRouter */}
+      <BrowserRouter>
+        <div className="container mx-auto p-4">
+          <Routes>
+            <Route path="/staff" element={<StaffTable />} />
+            <Route path="/staff/:staffId" element={<StaffDetailPage />} />
+            <Route index element={<StaffTable />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </>
   );
 }
