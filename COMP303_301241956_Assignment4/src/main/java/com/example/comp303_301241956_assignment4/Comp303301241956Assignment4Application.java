f527a76f1974e9b660591a7d0e7c09c96280f922
Sebastian <sebastian_bdavid@hotmail.com>
2025-04-08 14:37:47 -0400

    feat: enabled eureka discovery client

--- COMP303_301241956_Assignment4/src/main/java/com/example/comp303_301241956_assignment4/Comp303301241956Assignment4Application.java
+++ COMP303_301241956_Assignment4/src/main/java/com/example/comp303_301241956_assignment4/Comp303301241956Assignment4Application.java
@@ -3,8 +3,9 @@ package com.example.comp303_301241956_assignment4;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.CrossOrigin;
-
+@EnableDiscoveryClient
 @SpringBootApplication( exclude = {SecurityAutoConfiguration.class})
 @CrossOrigin(origins = "*")
 public class Comp303301241956Assignment4Application {
