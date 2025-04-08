4c26ff7f859a5e21c503cf82feca0fdeed7279e3
Sebastian <sebastian_bdavid@hotmail.com>
2025-04-08 14:27:25 -0400

    feat: setup eureka server

---
+++ EurekaServer_Test/src/test/java/com/example/eurekaserver_test/EurekaServerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurekaserver_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
