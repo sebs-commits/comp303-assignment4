89d7e4556a6770cd3d244a23d5c60f603db789d4
Sebastian <sebastian_bdavid@hotmail.com>
2025-04-08 17:02:22 -0400

    feat: setup spring security, for now were only authorizing those in MANAGEMENT department

---
+++ COMP303_301241956_Assignment4/src/main/java/com/example/comp303_301241956_assignment4/auth/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.example.comp303_301241956_assignment4.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/staff/", "/api/create-staff", "/api/update-staff/**").hasAuthority("MANAGEMENT")
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
